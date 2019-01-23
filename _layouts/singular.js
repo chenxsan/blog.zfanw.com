41981e3bfc9879c3c5deeffd0e67a8f8b7b2f41b
chenxsan <chenxsan@gmail.com>
2019-01-23 22:28:11 +0800

    Remove publisher

--- _layouts/singular.js
+++ _layouts/singular.js
@@ -77,10 +77,6 @@ module.exports = function ({
           "author": {
             "@type": "Person",
             "name": "${author}"
-          },
-          "publisher": {
-            "@type": "Person",
-            "name": "${author}"
           }
         }
       </script>
