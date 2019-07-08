90bc2439962d8b64ed0c53f471e11314ec309835
chenxsan <chenxsan@gmail.com>
2019-07-08 09:36:50 +0800

    Clean theme folder

--- themes/@okmarvin/january/src/components/Header.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Header from './Header'
-import renderer from 'react-test-renderer'
-it('renders correctly', () => {
-  const tree = renderer.create(<Header siteTitle={'okmarvin'} />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
--- themes/@okmarvin/january/src/components/Meta.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Meta from './Meta'
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Meta author='sam chen' theme='@okmarvin/january' />, div)
-})
